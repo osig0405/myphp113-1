e39e9faf572fe44573a1b92e996f50507533516e
Jack Chou <deidei1144@gmail.com>
2024-12-19 11:06:32 +0800

    essage

--- 696/ch08/ch8-4-2 copy.php
+++ 696/ch08/ch8-4-2 copy.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>ch8-4-2.php</title>
-</head>
-<body>
+
 <?php
-$id = $_GET["Id"];  //  取得URL參數
+$id = $_GET["Id"];  
 $name = $_GET["Name"];
-// 顯示參數值
 echo "學號: " . $id . "<br/>姓名: " . $name . "<br/>";
 ?>
-</body>
-</html>
