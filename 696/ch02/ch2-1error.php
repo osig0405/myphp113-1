a70df290cd20cf0f4272a0123e8e8e69ae8a65a3
Jack Chou <deidei1144@gmail.com>
2024-12-19 10:10:03 +0800

    essage

--- 696/ch02/ch2-1error.php
+++ 696/ch02/ch2-1error.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8" />
-<title>ch2-1error.php</title>
+    <meta charset="utf-8" />
+    <title>ch2-1error.php</title>
 </head>
+
 <body>
-<?php
-begin = 12;
-$end = 18;
-for ($i = $begin; $i <= $end; $i += 3) {
-?>
-<p style="font-size:<?php echo $i;?>pt">
-我的第一個PHP程式
-<?php
-print "</p>";
-} ?>
+    <?php
+    $begin = 12;
+    $end = 18;
+    for ($i = $begin; $i <= $end; $i += 3) {
+    ?>
+        <p style="font-size:<?php echo $i; ?>pt">
+            我的第一個PHP程式
+        <?php
+        print "</p>";
+    } ?>
 </body>
+
 </html>
